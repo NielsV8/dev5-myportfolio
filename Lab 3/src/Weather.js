af0cf9cf7277f88ea9c88c45a09d449fa23996de
NielsV8 <niels.verdickt@outlook.com>
2022-10-06 10:03:25 +0200

    feat: add weather api

---
+++ Lab 3/src/Weather.js
@@ -0,0 +1,41 @@
+export default class Weather {
+  constructor(api_key) {
+    this.apiKey = api_key;
+    this.getLocation();
+  }
+
+  getLocation(){
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(
+            this.getWeather.bind(this)
+            );
+    }else{
+        alert('Geolocation not supported');
+    }
+  }
+
+  getWeather(position){
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+
+        const url = `https://api.weatherapi.com/v1/current.json?key=${this.apiKey}&q=${lat},${lon}&aqi=no`;
+        fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            this.displayWeather(data);
+        });
+    }
+
+    displayWeather(data){
+        const temp = data.current.temp_c;
+        document.querySelector(".weather__temp").innerText = temp + "°C";
+
+        const weather = data.current.condition.text;
+        document.querySelector(".weather__condition").innerText = weather;
+
+        const icon = data.current.condition.icon;
+        const img = document.createElement('img');
+        img.src = icon;
+        document.querySelector(".weather__icon").appendChild(img);
+    }
+}
