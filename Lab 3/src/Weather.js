fe06b08749c21e5c0140473170a35d373a3ed510
Niels Verdickt <niels.verdickt@outlook.com>
2022-10-07 21:42:57 +0200

    feat: add weather boost api

--- Lab 3/src/Weather.js
+++ Lab 3/src/Weather.js
@@ -2,6 +2,7 @@ export default class Weather {
   constructor(api_key) {
     this.apiKey = api_key;
     this.getLocation();
+    this.getBoost();
   }
 
   getLocation(){
@@ -22,6 +23,7 @@ export default class Weather {
         fetch(url)
         .then(response => response.json())
         .then(data => {
+          console.log(data)
             this.displayWeather(data);
         });
     }
@@ -37,5 +39,29 @@ export default class Weather {
         const img = document.createElement('img');
         img.src = icon;
         document.querySelector(".weather__icon").appendChild(img);
+
+        if(weather == "Clear"){
+          this.displayClear(data);
+        }
+
+    }
+
+    getBoost(){
+      const url = `https://pogoapi.net/api/v1/weather_boosts.json`
+      fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.Clear);
+        this.displayClear(data);
+      });
+    }
+
+    displayClear(data){
+      const clear1 = data.Clear[0];
+      const clear2 = data.Clear[1];
+      const clear3 = data.Clear[2];
+      document.querySelector(".clear1").innerText = clear1;
+      document.querySelector(".clear2").innerText = clear2;
+      document.querySelector(".clear3").innerText = clear3;
     }
 }
