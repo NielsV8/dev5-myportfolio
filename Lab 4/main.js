b6eb7f51bdefd7cb12431d1a666e788efdc7779d
Niels Verdickt <niels.verdickt@outlook.com>
2022-10-14 11:06:55 +0200

    feat: add roof and background

--- Lab 4/main.js
+++ Lab 4/main.js
@@ -4,7 +4,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
       const scene = new THREE.Scene();
-      scene.background = new THREE.Color(0xfffffff);
+      scene.background = new THREE.Color(0x87CEEB);
 			const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 			const renderer = new THREE.WebGLRenderer();
@@ -14,14 +14,12 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
       const controls = new OrbitControls( camera, renderer.domElement );
       const textureLoader = new THREE.TextureLoader();
 
-      const wallTexture = textureLoader.load( "./assests/wall.jpg" );
       const planeGeometry = new THREE.PlaneGeometry( 1, 1 );
-      const planeMaterial = new THREE.MeshBasicMaterial( {color: 0xffffff, side: THREE.DoubleSide} );
+      const planeMaterial = new THREE.MeshBasicMaterial( {color: 0xAA4A44, side: THREE.DoubleSide} );
       const planeMesh = new THREE.Mesh( planeGeometry, planeMaterial );
       const planeMesh2 = new THREE.Mesh( planeGeometry, planeMaterial );
       const planeMesh3 = new THREE.Mesh( planeGeometry, planeMaterial );
       const planeMesh4 = new THREE.Mesh( planeGeometry, planeMaterial );
-      planeMaterial.map = wallTexture;
       planeMesh.position.set(0.5, 0, 0);
       planeMesh.rotation.y = 90 * Math.PI / 180;
       planeMesh2.position.set(0, 0, -0.5);
@@ -59,6 +57,16 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
       scene.add( floorMesh3 );
       scene.add( floorMesh4 );
 
+      //add ground
+      const groundTexture = textureLoader.load( "./assests/grass.jpg" );
+      const groundGeometry = new THREE.PlaneGeometry( 25, 25 );
+      const groundMaterial = new THREE.MeshBasicMaterial( {color: 0xffffff, side: THREE.DoubleSide} );
+      const groundMesh = new THREE.Mesh( groundGeometry, groundMaterial );
+      groundMesh.position.y = -0.51;
+      groundMesh.rotation.x = Math.PI / 2;
+      groundMaterial.map = groundTexture;
+      scene.add( groundMesh );
+
       //add card
       const cardTexture = textureLoader.load( "./assests/logo.jpg" );
       const cardGeometry = new THREE.CircleGeometry( 0.2, 32 );
@@ -68,18 +76,19 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
       cardMesh.position.set(0, 0, -0.49);
       scene.add( cardMesh );
 
-      //add roof
-      const roofTexture = textureLoader.load( "./assests/roof.jpg" );
-      const roofGeometry = new THREE.PlaneGeometry( 1, 1 );
-      const roofMaterial = new THREE.MeshBasicMaterial( {color: 0xffffff, side: THREE.DoubleSide} );
-      const roofMesh = new THREE.Mesh( roofGeometry, roofMaterial );
-      roofMaterial.map = roofTexture;
-      roofMesh.position.set(0, 0.5, 0);
-      roofMesh.rotation.x = Math.PI / 2;
-      scene.add( roofMesh );
-
       //add roof cone
-      const light = new THREE.DirectionalLight( 0xffff00, 1 );
+      const roofTexture = textureLoader.load( "./assests/roof.jpg" );
+      const roofConeGeometry = new THREE.ConeGeometry( 1.5, 1, 4 );
+      const roofConeMaterial = new THREE.MeshBasicMaterial( {color: 0xffffff, side: THREE.DoubleSide} );
+      const roofConeMesh = new THREE.Mesh( roofConeGeometry, roofConeMaterial );
+      roofConeMaterial.map = roofTexture;
+      roofConeMesh.position.set(-0.5, 1, 0.5);
+      roofConeMesh.rotation.y = 45 * Math.PI / 180;
+      scene.add( roofConeMesh );
+
+      //add light
+      const light = new THREE.PointLight( 0xffffff, 1, 100 );
+      light.position.set( 0, 0, 0 );
       scene.add( light );
 
 			camera.position.z = 5;
@@ -87,8 +96,6 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 			function animate() {
 				requestAnimationFrame( animate );
 				renderer.render( scene, camera );
-
-        
 			};
 
 			animate();
